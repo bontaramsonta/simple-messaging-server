7d7808d63c3ed6992e666febe8258f327dcf48c4
bontaramsonta <18cs113@gmail.com>
2023-09-18 22:40:00 +0530

    wip: add persistence using redis db

--- main.ts
+++ main.ts
@@ -1,9 +1,14 @@
 import { createSigner, createVerifier } from "fast-jwt";
 import { createClient } from "redis";
 
+//util
 const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
 const l = (...args: any[]) => console.log("[MX]", ...args);
+// ---
 
+// types
+
+// jwt
 const tokenSigner = createSigner({
   algorithm: "HS256",
   key: "secret",
@@ -23,16 +28,16 @@ const tokenVerifier = createVerifier({
 });
 
 function createToken(payload: {
-  userId: string;
-  username: string;
-  avatar: string;
+  id: string;
 }) {
   return tokenSigner(payload);
 }
 function verifyToken(token: string) {
   return tokenVerifier(token);
 }
+// ---
 
+// redis
 const redis = createClient();
 redis.once("ready", () => {
   console.log("redis connected");
@@ -41,25 +46,92 @@ redis.once("error", (err) => {
   console.error("redis error", err);
 });
 
-await redis.connect();
+type MessageContext = "dm" | "room";
 
-interface WSData {
+interface Message {
+  id: string;
+  from: string;
+  to: string;
+  content: string;
+  date: number;
+  context: MessageContext;
+  isDeleted: boolean;
+  isRead: boolean;
+}
+
+interface User {
   id: string;
   username: string;
   avatar: string;
   rooms: string[];
+  friends: string[];
+  isOnline: boolean;
+  lastSeen: number;
+}
+
+async function saveMessage(message: Message) {
+  return await redis.json.set(
+    `message:${message.id}`,
+    ".",
+    JSON.stringify(message),
+  );
+}
+
+async function getMessage(id: string) {
+  return JSON.parse(await redis.json.get(`message:${id}`) as string) as Message;
+}
+
+async function getUser(id: string) {
+  return JSON.parse(await redis.json.get(`user:${id}`) as string) as User;
+}
+
+async function insertUserIntoRoom(userId: string, roomId: string) {
+  return await redis.json.arrAppend(`user:${userId}`, ".rooms", roomId);
+}
+
+async function removeUserFromRoom(
+  userId: string,
+  roomId: string,
+): Promise<string | null> {
+  const index = await redis.json.arrIndex(`user:${userId}`, ".rooms", roomId);
+  if (typeof index === "number") {
+    console.log("roomId not found");
+    return null;
+  } else {
+    console.log("index", index);
+    return await redis.json.arrPop(
+      `user:${userId}`,
+      ".rooms",
+      index[0],
+    ) as string;
+  }
+}
+
+async function setUserOnlineStatus(userId: string, isOnline: boolean) {
+  return await redis.json.set(
+    `user:${userId}`,
+    ".isOnline",
+    isOnline,
+  );
+}
+
+await redis.connect();
+// ---
+
+interface WSData {
+  id: string;
 }
 
 type WSMessage = {
   type: "message";
-  context: "dm" | "room";
+  context: MessageContext;
   to: string;
   from: string;
   content: string;
   date: number;
 } | {
   type: "typing";
-  context: "dm" | "room";
+  context: MessageContext;
   to: string;
   from: string;
 };
@@ -75,28 +147,20 @@ const server = Bun.serve<WSData>({
       }
       const token = search.get("auth")!;
       const { payload } = verifyToken(token);
-      const rooms = search.get("rooms")?.split(",") ?? [];
-
       const success = server.upgrade(req, {
         data: {
-          id: payload.userId,
-          username: payload.username,
-          avatar: payload.avatar,
-          rooms,
+          id: payload.id,
         },
       });
       return success
         ? undefined
         : new Response("WebSocket upgrade error", { status: 400 });
     }
+    //token create
     if (req.method === "POST" && url.pathname === "/token") {
-      const body = await req.json(); // id, username, avatar?
+      const body = await req.json(); // id
 
-      const token = createToken({
-        userId: body.id,
-        username: body.username,
-        avatar: body?.avatar ?? "",
-      });
+      const token = createToken({ id: body.id });
       return new Response(JSON.stringify({ token }));
     }
 
@@ -104,24 +168,32 @@ const server = Bun.serve<WSData>({
   },
   websocket: {
     backpressureLimit: 1024 * 1024 * 1024, // 1GB
-    open(ws) {
-      console.log(`${ws.data.id} has entered the chat`);
-      ws.subscribe(`user:${ws.data.id}`);
-      // sub to rooms
-      ws.data.rooms.forEach((room) => ws.subscribe(`room:${room}`));
-      // publish in all rooms that user is online
-      ws.data.rooms.forEach((room) =>
-        ws.publish(
-          `room:${room}`,
-          JSON.stringify({
-            type: "user-online",
-            userId: ws.data.id,
-            username: ws.data.username,
-            avatar: ws.data.avatar,
-          }),
-        )
-      );
+    //open ws
+    async open(ws) {
+      try {
+        // get user for db
+        const user = await getUser(ws.data.id);
+        console.log(`${user.username} has entered the chat`);
+        ws.subscribe(`user:${user.id}`);
+        // sub to rooms
+        user.rooms.forEach((room) => ws.subscribe(`room:${room}`));
+        // publish in all rooms that user is online
+        user.rooms.forEach((room) =>
+          ws.publish(
+            `room:${room}`,
+            JSON.stringify({
+              type: "user-online",
+              userId: user.id,
+              username: user.username,
+              avatar: user.avatar,
+            }),
+          )
+        );
+      } catch (err) {
+        console.error("[ws open error]", ws.data.id, err);
+      }
     },
+    //message ws
     message(ws, message: string) {
       try {
         console.log("message from", ws.data.id, ":", message);
@@ -129,15 +201,28 @@ const server = Bun.serve<WSData>({
         const msg = JSON.parse(message) as WSMessage;
         // send message event
         if (msg.type === "message") {
+          // save message
+          const msgId = `${msg.context}:${msg.from}:${msg.to}:${Date.now()}`;
+          saveMessage({
+            id: msgId,
+            from: msg.from,
+            to: msg.to,
+            content: msg.content,
+            date: msg.date,
+            context: msg.context,
+            isDeleted: false,
+            isRead: false,
+          });
           if (msg.context === "dm") {
             console.log("sending dm to", msg.to);
             l(ws.publish(
               `user:${msg.to}`,
               JSON.stringify({
                 type: "message",
+                id: msgId,
                 from: ws.data.id,
                 content: msg.content,
-                date: Date.now(),
+                date: msg.date,
               }),
             ));
           } else {
@@ -146,13 +231,14 @@ const server = Bun.serve<WSData>({
               `room:${msg.to}`,
               JSON.stringify({
                 type: "message",
+                id: msgId,
                 from: ws.data.id,
                 content: msg.content,
                 date: Date.now(),
               }),
             ));
           }
-          // typing indication event
+          // send typing event
         } else if (msg.type === "typing") {
           if (msg.context === "dm") {
             console.log("sending dm typing to", msg.to);
@@ -175,18 +261,22 @@ const server = Bun.serve<WSData>({
           }
         }
       } catch (err) {
-        console.error(err);
+        console.error("[ws message error]", ws.data.id, err);
       }
     },
-    close(ws) {
-      console.log(`${ws.data.username} has left the chat`);
+    //close ws
+    async close(ws) {
+      const user = await getUser(ws.data.id);
+      console.log(`${user.username} has left the chat`);
+      // set user offline
+      setUserOnlineStatus(user.id, false);
       // publish in all rooms that user is offline
-      ws.data.rooms.forEach((room) =>
+      user.rooms.forEach((room) =>
         ws.publish(
           `room:${room}`,
           JSON.stringify({
             type: "user-offline",
-            userId: ws.data.id,
+            userId: user.id,
           }),
         )
       );
